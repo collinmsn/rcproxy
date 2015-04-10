ef5c4b50d67b6e6dfeb5689b249f11871838efb0
collinmsn <4130944@qq.com>
2015-04-10 13:44:31 +0800

    add travis ci

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,6 @@ godep path > /dev/null 2>&1
 if [ "$?" = 0 ]; then
     GOPATH=`godep path`:$GOPATH
     godep restore
-    make || exit $?
-    make gotest
     exit 0
 else
     echo "godep is not found, @see https://github.com/tools/godep"
